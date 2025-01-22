6654b35ae3272eb3ad963fd9dd3e75bf8e7e33ae
Jostein24sfj <jostein24sfj@gmail.com>
2025-01-22 14:46:59 +0100

    kommentert ut app og index css

--- src/Components/BlogPreviewCard/BlogPreviewCard.jsx
+++ src/Components/BlogPreviewCard/BlogPreviewCard.jsx
@@ -1,10 +1,27 @@
 import "./blogPreviewCard.css"
+import avatarImage from "./image-avatar.webp"
+import illustrationImage from "./illustration-article.svg"
 
 export function BlogPreviewCard(){
     return (
         
-        <div>
-            tittel
+        <div class= "card">
+            <header>
+            <img class="topImg" src={illustrationImage} alt="" />
+            <h1>Learning</h1>
+            <p>Produsert 29.10.2024</p>
+            </header>
+
+            <section>
+            <h2>HTML & CSS foundation</h2>
+            <p>These languages are the backbone of every website,
+               defining structure, content and presentation.</p>
+            </section>
+            
+            <footer>
+            <img src={avatarImage} alt="" />
+             <h3>Greg Hopper</h3>
+            </footer>
         </div>
             
     )
