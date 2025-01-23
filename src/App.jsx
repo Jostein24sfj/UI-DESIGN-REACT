edd259b2c6c17954a3fddbf6e25f6965c29899be
Jostein24sfj <jostein24sfj@gmail.com>
2025-01-23 13:38:48 +0100

    finished BlogPreviewCard

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     <section>
       <BlogPreviewCard/>
     </section>
+    
 
     <section>
       <RecipePage/>
--- src/Components/BlogPreviewCard/BlogPreviewCard.jsx
+++ src/Components/BlogPreviewCard/BlogPreviewCard.jsx
@@ -1,6 +1,6 @@
 import styles from "./blogPreviewCard.module.css";
 import avatarImage from "./image-avatar.webp";
-import illustrationImage from "./illustration-article.svg"; // add desktop image instead
+import illustrationImage from "./illustration-article.svg"; 
 
 export function BlogPreviewCard() {
   return (
@@ -9,18 +9,16 @@ export function BlogPreviewCard() {
         <img className={styles.topImg} src={illustrationImage} alt="" />
         <div className={styles.cardContent}>
           <h3>Learning</h3>
-          {/* endre teksten under så den er lik designet:  */}
-          <p>Produsert 23.01.2025</p>
+          <p>Published 21 Dec 2023</p>
 
-          <h2>HTML & CSS foundation</h2>
-          {/* fiks className så du henter fra styles variablene (samme som de andre classNamene det har blitt gjort på) */}
-          <p className="description">
+          <h2>HTML & CSS foundations</h2>
+          <p className={styles.description}>
             These languages are the backbone of every website, defining
-            structure, content and presentation.
+            structure, content, and presentation.
           </p>
         <div className={styles.author}>
           <img src={avatarImage} alt="" />
-          <h4>Greg Hopper</h4>
+          <h4>Greg Hooper</h4>
         </div>
         </div>
       </div>
