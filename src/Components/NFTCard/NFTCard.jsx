9210e6ec63c662f05ef185fc4908116558107a95
Jostein24sfj <jostein24sfj@gmail.com>
2025-01-24 10:20:04 +0100

    fixed html structure on NFT card

--- src/Components/NFTCard/NFTCard.jsx
+++ src/Components/NFTCard/NFTCard.jsx
@@ -11,13 +11,11 @@ export function NFTCard() {
             <h3>Equilibrium #3429</h3>
             <p>Our Equilibrium collection promotes balance and calm</p>
 
-            <div className="crypto">
-              <p>0.0041 ETH</p>
-            </div>
-
-             <div className={styles.numbers}>
-              <p>3 days left</p>
-             </div>
+            
+              <p className={styles.crypto}>0.0041 ETH</p>
+            
+              <p className={styles.numbers}>3 days left</p>
+             
 
             <div className={styles.author}>
              <img src={avatarImage} alt="" />
