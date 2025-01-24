a912b14fadcc62e84f8d6d632ca590bfda488ad5
Jostein24sfj <jostein24sfj@gmail.com>
2025-01-24 11:38:50 +0100

    Finished NFTCard and added icons

--- src/Components/NFTCard/NFTCard.jsx
+++ src/Components/NFTCard/NFTCard.jsx
@@ -1,28 +1,37 @@
 import styles from "./NFTCard.module.css";
-import image from "./image-equilibrium.jpg"
-import avatarImage from "./image-avatar.png"
+import image from "./image-equilibrium.jpg";
+import avatarImage from "./image-avatar.png";
+import { IconClock } from "./icon-clock";
+import { IconEth } from "./icon-eth";
 
 export function NFTCard() {
-    return (
+  return (
     <>
-        <div className={styles.card}>
-         <img className={styles.topImg} src={image} alt="" />
-          <div className={styles.cardContent}>
-            <h3>Equilibrium #3429</h3>
-            <p>Our Equilibrium collection promotes balance and calm</p>
+      <div className={styles.card}>
+        <img className={styles.topImg} src={image} alt="" />
+        <div className={styles.cardContent}>
+          <h3>Equilibrium #3429</h3>
+          <p>Our Equilibrium collection promotes balance and calm.</p>
 
-            
-              <p className={styles.crypto}>0.0041 ETH</p>
-            
-              <p className={styles.numbers}>3 days left</p>
-             
+          <div className={styles.price}>
+            <p className={styles.crypto}>
+              <IconEth />
+              0.041 ETH
+            </p>
+            <p className={styles.numbers}>
+              {" "}
+              <IconClock />3 days left
+            </p>
+          </div>
 
-            <div className={styles.author}>
-             <img src={avatarImage} alt="" />
-             <h4>Creation of Jules Wyvern</h4>
-            </div>
+          <div className={styles.author}>
+            <img src={avatarImage} alt="" />
+            <h4>
+              Creation of <span>Jules Wyvern</span>
+            </h4>
           </div>
         </div>
+      </div>
     </>
-    );
+  );
 }
