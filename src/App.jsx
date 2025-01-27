aac467c742ff3dfa90152f44f20c91d091d541bd
vegar24sfj <vegar24sfj@gmail.com>
2025-01-27 13:48:20 +0100

    changes in app.jsx/css with router

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,52 @@ import { BlogPreviewCard } from "./Components/BlogPreviewCard/BlogPreviewCard";
 import "./App.css";
 import { RecipePage } from "./components/RecipePage/RecipePage";
 import { ResultsSummaryComponent } from "./components/ResultsSummaryComponent/ResultsSummaryComponent";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <header></header>
-      <main>
+      <Router>
+        {/* Header Section with Navigation Links */}
+        <header>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/recipe">Recipe</Link></li>
+              <li><Link to="/results">Results</Link></li>
+            </ul>
+          </nav>
+        </header>
 
-    <section>
-      <BlogPreviewCard/>
-    </section>
+        {/* Routes for Different Pages */}
+        <Routes>
+          <Route path="/" element={<BlogPreviewCard />} />
+          <Route path="/recipe" element={<RecipePage />} />
+          <Route path="/results" element={<ResultsSummaryComponent />} />
+        </Routes>
 
-    <section>
-      <RecipePage/>
-    </section>
+        {/* Main Section with Content */}
+        <main>
+          <section>
+            <BlogPreviewCard />
+          </section>
 
-    <section>
-      <ResultsSummaryComponent/>
-    </section>
+          <section>
+            <RecipePage />
+          </section>
 
-  </main>
-    <footer></footer>
+          <section>
+            <ResultsSummaryComponent />
+          </section>
+        </main>
+
+        {/* Footer */}
+        <footer>
+          <p>© 2025 Your Company</p>
+        </footer>
+      </Router>
     </>
-  )
+  );
 }
 
 export default App;
